1912b7e1213688154bc09673c2c0d1f7bf4c0349
Alan Phan <alanphanxyz@gmail.com>
2022-09-19 18:09:57 +0700

    first commit

---
+++ django_auth/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import UserDetailView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("auth/", include("dj_rest_auth.urls")),
+    path("auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("me/", UserDetailView.as_view()),
+]
